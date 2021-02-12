7e200852e5f3a77f5cee29e79b56fd2a1817c39f
Nick Allen <nwestallen@gmail.com>
2021-02-12 12:14:50 -0500

    added tests for render & success message

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { getByText, render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+
+    userEvent.type(firstNameInput,'Johnboy');
+    userEvent.type(lastNameInput, 'Namesman');
+    userEvent.type(addressInput, '11223');
+    userEvent.type(cityInput, 'Townsville');
+    userEvent.type(stateInput, 'Ohio');
+    userEvent.type(zipInput, '43242');
+
+    const button = screen.getByRole('button', {name: /checkout/i});
+
+    userEvent.click(button);
+
+    const successMessage = /you have ordered some plants! woo-hoo!/i
+
+    expect(screen.getByText(successMessage)).toBeInTheDocument();
+});
